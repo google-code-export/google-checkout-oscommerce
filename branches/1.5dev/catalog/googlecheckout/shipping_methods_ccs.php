aa60ac23aee39b2967f418db68d43d84814ccfb5
ed.davisson <ed.davisson@35141ac2-221b-0410-bcd0-732de11ca7f3>
2008-12-18 21:49:00 +0000

    Cleaning up the shipping_methods*.php files

--- branches/1.5dev/catalog/googlecheckout/shipping_methods_ccs.php
+++ branches/1.5dev/catalog/googlecheckout/shipping_methods_ccs.php
@@ -1,62 +1,65 @@
 <?php
-/*
- **GOOGLE CHECKOUT ** v1.4.5
- * @version $Id: shipping_methods_ccs.php 5862 2007-09-19 14:58:57Z ropu $
- * Is recommended you DONT change this file unless you know what you are doing
+
+/**
+ * Carrier Calculated Shipping Methods.
+ *
+ * These must be kept in sync with the shipping companies and shipping types
+ * supported by the Google Checkout Carrier Calculated Shipping XML API.
+ * 
+ * For more information, see:
+ * 
+ *   {@link http://code.google.com/apis/checkout/developer/Google_Checkout_XML_API_Carrier_Calculated_Shipping#tag_shipping-company}
+ * 
+ * and: 
+ * 
+ *   {@link http://code.google.com/apis/checkout/developer/Google_Checkout_XML_API_Carrier_Calculated_Shipping#tag_shipping-type}
  */
-	$cc_shipping_methods = array(
-                        'fedex' => array(
-                                    'domestic_types' =>
-                                      array(
-                                          'Ground' => 'Ground',
-                                          'Home Delivery' => 'Home Delivery',
-                                          'Express Saver' => 'Express Saver',
-                                          '2Day' => '2Day',
-                                          'Standard Overnight' => 'Standard Overnight',
-                                          'Priority Overnight' => 'Priority Overnight',
-                                          'First Overnight' => 'First Overnight',
-                                           ),
 
-                                    'international_types' =>
-                                      array(
+$cc_shipping_methods = array(
+  'fedex' => array(
+    'domestic_types' => array(
+      'Ground' => 'Ground',
+      'Home Delivery' => 'Home Delivery',
+      'Express Saver' => 'Express Saver',
+      '2Day' => '2Day',
+      'Standard Overnight' => 'Standard Overnight',
+      'Priority Overnight' => 'Priority Overnight',
+      'First Overnight' => 'First Overnight',
+    ),
+    'international_types' => array(
+    ),
+  ),
+  'ups' => array(
+    'domestic_types' => array(
+      'Ground' => 'Ground',
+      '3 Day Select' => '3 Day Select',
+      '2nd Day Air' => '2nd Day Air',
+      // TODO(eddavisson): This was commented out before. Test that it works.
+      '2nd Day Air AM' => ' 2nd Day Air AM',
+      'Next Day Air Saver' => 'Next Day Air Saver',
+      'Next Day Air' => 'Next Day Air',
+      'Next Day Air Early AM' => 'Next Day Air Early AM',
+    ),
+    'international_types' => array(
+    ),
+  ),
+  'usps' => array(
+    'domestic_types' => array(
+      'Media Mail' => 'Media Mail',
+      'Parcel Post' => 'Parcel Post',
+      'Express Mail' => 'Express Mail',
+      // TODO(eddavisson): This was commented out before. Test that it works.
+      'Priority Mail' => 'Priority Mail',
+    ),
+    'international_types' => array(
+    ),
+  ),
+);   
+                                                                                     
+$cc_shipping_methods_names = array(
+  'fedex' => 'FedEx',
+  'ups' => 'UPS',
+  'usps' => 'USPS',
+);
 
-                                           ),
-                                        ),
-                        'ups' => array(
-                                    'domestic_types' =>
-                                      array(
-                                          'Ground' => 'Ground',
-                                          '3 Day Select' => '3 Day Select',
-                                          '2nd Day Air' => '2nd Day Air',
-//                                          '2nd Day Air AM' => ' 2nd Day Air AM',
-                                          'Next Day Air Saver' => 'Next Day Air Saver',
-                                          'Next Day Air' => 'Next Day Air',
-                                          'Next Day Air Early AM' => 'Next Day Air Early AM',
-                                           ),
-
-                                    'international_types' =>
-                                      array(
-   
-                                           ),
-                                        ),
-                        'usps' => array(
-                                    'domestic_types' =>
-                                      array(
-                                          'Media Mail' => 'Media Mail',
-                                          'Parcel Post' => 'Parcel Post',
-                                          'Express Mail' => 'Express Mail',
-//                                          'Priority Mail' => 'Priority Mail',
-                                           ),
-
-                                    'international_types' =>
-                                      array(
-
-                                           ),
-                                        )
-                                    );     
-                                                                                       
-  $cc_shipping_methods_names = array(    'fedex' => 'FedEx',
-                                         'ups' => 'UPS',
-                                         'usps' => 'USPS',
-                                        );
 ?>

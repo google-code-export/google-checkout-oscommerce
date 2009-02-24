ee013c930488d996b8ade1ec7c6ce758132f4b03
alek.dembowski <alek.dembowski@35141ac2-221b-0410-bcd0-732de11ca7f3>
2009-02-24 17:27:22 +0000

    Added support for py2exe so the user doesn't need python installed.

--- branches/1.5dev/install.bat
+++ branches/1.5dev/install.bat
@@ -1,13 +1,12 @@
 @setlocal 
 
-REM This script wraps the python install script
-
 @echo off
+REM This runs the install script
 echo Running the installer
 
 set target_dir=%1
-set installer=python tools\installer.py
-REM set installer=tools\windows\installer.exe
+REM set installer=python tools\installer.py
+set installer=tools\windows\dist\installer.exe
 
 if defined target_dir (
   %installer% --diff3=tools\windows\diffutils\bin\diff3.exe --zip_backup catalog\ tools\golden\oscommerce-2.2rc2a\catalog\ %target_dir%\

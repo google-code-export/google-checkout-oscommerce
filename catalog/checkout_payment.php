146cdf08acca6d55681d88351df373127103de62
ed.davisson <ed.davisson@35141ac2-221b-0410-bcd0-732de11ca7f3>
2009-03-09 20:50:04 +0000

    Revert a couple of spurious changes to the OSC files.

--- catalog/checkout_payment.php
+++ catalog/checkout_payment.php
@@ -1,20 +1,13 @@
 <?php
 /*
-  Copyright (C) 2007 Google Inc.
+  $Id$
 
-  This program is free software; you can redistribute it and/or
-  modify it under the terms of the GNU General Public License
-  as published by the Free Software Foundation; either version 2
-  of the License, or (at your option) any later version.
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
 
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+  Copyright (c) 2007 osCommerce
 
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+  Released under the GNU General Public License
 */
 
   require('includes/application_top.php');
